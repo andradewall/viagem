9caa2aaec454ff54a11bb09cbba0fb48df4bf855
Wallace Andrade <andradewall0@gmail.com>
2024-05-05 12:05:17 -0300

    send files

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}"
+    x-data="tallstackui_darkTheme()"
+    x-bind:class="{ 'dark bg-gray-700': darkTheme, 'bg-sky-50': !darkTheme }">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>{{ $title ?? 'Page Title' }}</title>
+
+        <tallstackui:script />
+        @livewireStyles
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+    <body class="text-sky-950 dark:text-white/50 max-w-screen-lg mx-auto">
+        @include('layouts.navbar')
+
+        {{ $slot }}
+
+        @livewireScript
+    </body>
+</html>
---
+++ resources/views/tags/index.blade.php
@@ -0,0 +1,3 @@
+<x-layouts.app>
+    Teste
+</x-layouts.app>
